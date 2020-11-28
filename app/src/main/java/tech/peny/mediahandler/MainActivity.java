71f146e66a8525b0ad5d30b91ca7bafe8dc71008
princenyamadi <princenyamadi.pn@gmail.com>
2020-11-28 11:37:55 +0000

    [feat]: Added layout and function
    Added a VideoView widget to play videos and wrote states that added video to the video files to the video view and set the media controller to control the video

--- app/src/main/java/tech/peny/mediahandler/MainActivity.java
+++ app/src/main/java/tech/peny/mediahandler/MainActivity.java
@@ -3,12 +3,28 @@ package tech.peny.mediahandler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+
+        videoView.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.trail);
+
+//        Media Controller Config
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+
+//        Play Video
+        videoView.start();
     }
 }
