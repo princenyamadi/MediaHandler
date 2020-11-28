e12857165d3b5ef68823d4fc926a02d816e17b4f
princenyamadi <princenyamadi.pn@gmail.com>
2020-11-28 12:50:13 +0000

    [feat]: Added layout and code for controlling audio
    Added 2 buttons for play and pause, and 2 functions to play and pause

--- app/src/main/java/tech/peny/mediahandler/MainActivity.java
+++ app/src/main/java/tech/peny/mediahandler/MainActivity.java
@@ -2,29 +2,29 @@ package tech.peny.mediahandler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
+
+    public void play(View view){
+        mediaPlayer.start();
+    }
+
+    public void pause(View view){
+        mediaPlayer.pause();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.dancemonkey);
 
-        VideoView videoView = (VideoView) findViewById(R.id.videoView);
-
-        videoView.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.trail);
-
-//        Media Controller Config
-
-        MediaController mediaController = new MediaController(this);
-        mediaController.setAnchorView(videoView);
-        videoView.setMediaController(mediaController);
-
-//        Play Video
-        videoView.start();
     }
 }
